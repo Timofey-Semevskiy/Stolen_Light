90e9d90256e0f41b41ad0208b666a00f6a316da5
Timofey-Semevskiy <126121820+Timofey-Semevskiy@users.noreply.github.com>
2023-02-26 21:48:13 +0300

    add FlashLigth Vectors

--- LigthStolenAgain/Assets/Scripts/FlashLigthLogic.cs
+++ LigthStolenAgain/Assets/Scripts/FlashLigthLogic.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class FlashLigthLogic : MonoBehaviour
 {
+    [SerializeField] public GameObject character;
     [SerializeField] private GameObject flashLigth;
     [SerializeField] PlayerMovment player;
+    [SerializeField] private GameObject flashInHandPlayer;
+    
+
+
+    public float radius = 1f;
+    [Range(0, 1)]
+    public float angTresh = 0.5f;
     void Start()
     {
         
@@ -31,4 +40,24 @@ public class FlashLigthLogic : MonoBehaviour
             player.haveFlashLigth = true;
         }
     }
+    
+
+    private void OnDrawGizmos()
+    {
+     
+        Handles.DrawWireDisc(player.transform.position, Vector3.forward, radius);
+        float p = angTresh;
+        float x = Mathf.Sqrt(1 - p * p);
+
+        Vector3 vRigth = transform.right * p + transform.up * (-x);
+        Vector3 vDown = transform.right * p + transform.up * x;
+        Gizmos.DrawRay(flashInHandPlayer.transform.position, vRigth);
+        Gizmos.DrawRay(flashInHandPlayer.transform.position, vDown);
+
+    }
+
+    
+
 }
+
+
