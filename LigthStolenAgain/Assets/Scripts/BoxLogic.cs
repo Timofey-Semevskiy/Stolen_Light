a16c00319d6e68540962e1ce1079f6c99d8c48d7
Timofey-Semevskiy <126121820+Timofey-Semevskiy@users.noreply.github.com>
2023-03-12 08:41:41 +0300

    finally

--- LigthStolenAgain/Assets/Scripts/BoxLogic.cs
+++ LigthStolenAgain/Assets/Scripts/BoxLogic.cs
@@ -1,35 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BoxLogic : MonoBehaviour
 {
     public GameObject boxPrefab;
     public GameObject visibalBoxPrefab;
+    public ReternBack reternBack;   
 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       if(collision.gameObject.tag == "Flashlight")
+        
+        if (collision.gameObject.tag == "Flashlight")
         {
             Destroy(boxPrefab);
-            Instantiate(visibalBoxPrefab,boxPrefab.transform.position,Quaternion.identity);
-            
+            Debug.Log("DESTROY");
+            reternBack._visibalBoxPrefabSpawned = true;
+            Instantiate(visibalBoxPrefab, boxPrefab.transform.position, Quaternion.identity);
+
+
         }
     }
 
 
    
+   
+  
 }
--- LigthStolenAgain/Assets/Scripts/ReternBack.cs
+++ LigthStolenAgain/Assets/Scripts/ReternBack.cs
@@ -6,26 +6,24 @@ public class ReternBack : MonoBehaviour
 {
     public GameObject boxPrefab;
     public GameObject visibalBoxPrefab;
-    public bool _visibalBoxPrefabSpawned;
-    private void Start()
-    {
-        Retern();
-    }
+    public bool _visibalBoxPrefabSpawned = false;
+   
     void Update()
     {
+        if (_visibalBoxPrefabSpawned == true) {
+            StartCoroutine(Retern());
+            _visibalBoxPrefabSpawned = false;
+        }
         
     }
 
-
     IEnumerator Retern()
     {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-
-        //yield on a new YieldInstruction that waits for 5 seconds.
-        yield return new WaitForSeconds(5);
-
-        //After we have waited 5 seconds print the time again.
+        
+        yield return new WaitForSeconds(2);
+        Instantiate(boxPrefab, visibalBoxPrefab.transform.position, Quaternion.identity);
+        
+        Destroy(visibalBoxPrefab, 1f);
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
 
     }
